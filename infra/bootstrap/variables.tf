75f6891142fb04c6a38d9f733f5120b689cafbda
Tooltu <nttu2425@clc.fitus.edu.vn>
2026-07-13 21:14:43 +0700

    feat: bootstrap s3 bucket for s3 backend

---
+++ infra/bootstrap/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Application Region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "state_bucket_name" {
+  description = "Name for state bucket"
+  type        = string
+}
+

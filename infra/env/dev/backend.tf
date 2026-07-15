e03721b9ef0b0ca7c4186ff0a28066bc51b9cbb4
Tooltu <nttu2425@clc.fitus.edu.vn>
2026-07-15 21:19:26 +0700

    refactor: reformat terraform code

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "tooltu-streamforge-state"
-        region = "ap-southeast-1"
-        key = "env/dev/terraform.tfstate"
-        use_lockfile = true
-        encrypt = true
-    }
+  backend "s3" {
+    bucket       = "tooltu-streamforge-state"
+    region       = "ap-southeast-1"
+    key          = "env/dev/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
+  }
 }

64a203ab62cbd852856b165f193dbd2fbd8ccfc4
Tooltu <nttu2425@clc.fitus.edu.vn>
2026-07-15 21:23:58 +0700

    refactor: add required_version to env/dev

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54.0"
+    }
+  }
+
   backend "s3" {
     bucket       = "tooltu-streamforge-state"
     region       = "ap-southeast-1"

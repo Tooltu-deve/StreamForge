e11dca3295ab1b0d8a21b40b2e7d6c937c067312
Tooltu <nttu2425@clc.fitus.edu.vn>
2026-07-22 18:12:07 +0700

    feat: platform/dev layer wiring alb-controller

---
+++ infra/platform/dev/outputs.tf
@@ -0,0 +1,3 @@
+output "controller_role_arn" {
+  value = module.alb_controller.controller_role_arn
+}
---
+++ infra/platform/dev/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "name_prefix" {
+  type    = string
+  default = "streamforge-dev"
+}
+
+variable "core_state_bucket" {
+  type    = string
+  default = "tooltu-streamforge-state"
+}
+
+variable "core_state_key" {
+  type    = string
+  default = "env/dev/terraform.tfstate"
+}
+
+variable "lbc_chart_version" {
+  description = "aws-load-balancer-controller chart version (verify latest stable)"
+  type        = string
+  default     = "3.4.2"
+}
